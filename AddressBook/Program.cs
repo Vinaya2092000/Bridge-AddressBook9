bbd97817b5b15df896d34cd88c8ac9a036e20257
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-04 18:13:23 +0530

    [Vinaya]Add: Create

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 9 Address Book");
+            Console.WriteLine("Enter to create a contact");
+            Address obj = new Address();
+            obj.create();
+            obj.display();
         }
     }
 }
